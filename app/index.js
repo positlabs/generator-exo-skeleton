88aa3ce16858f835845ab6b6c7bcaf476ac542de
Josh <josh@toolofna.com>
2013-10-28 18:09:54 -0700

    trying to get this to update

--- app/index.js
+++ app/index.js
@@ -4,33 +4,34 @@ var path = require('path');
 var yeoman = require('yeoman-generator');
 
 var ExoSkeletonGenerator = module.exports = function ExoSkeletonGenerator(args, options, config) {
-  yeoman.generators.Base.apply(this, arguments);
+	yeoman.generators.Base.apply(this, arguments);
 
-  this.on('end', function () {
-    this.installDependencies({ skipInstall: options['skip-install'] });
-  });
+	this.on('end', function () {
+		this.installDependencies({ skipInstall: options['skip-install'] });
+	});
 
-  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+	this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+	console.log("exo-skeleton version " + this.pkg.version);
 };
 
 util.inherits(ExoSkeletonGenerator, yeoman.generators.Base);
 
 ExoSkeletonGenerator.prototype.askFor = function askFor() {
-  var cb = this.async();
+	var cb = this.async();
 
-  // have Yeoman greet the user.
-  console.log(this.yeoman);
-  cb();
+	// have Yeoman greet the user.
+	console.log(this.yeoman);
+	cb();
 };
 
 ExoSkeletonGenerator.prototype.app = function app() {
-  var cb = this.async();
+	var cb = this.async();
 
-  this.remote('positlabs', 'exo-skeleton', function(err, remote) {
-    console.log(remote.directory);
-    remote.directory('.', '.');
-    cb();
-  });
+	this.remote('positlabs', 'exo-skeleton', function (err, remote) {
+		console.log(remote.directory);
+		remote.directory('.', '.');
+		cb();
+	});
 
 };
 
